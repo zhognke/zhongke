4a35803de45f1570f023724d4f77966d44ae8d45
fjs1987 <yangying2762@qq.com>
2019-10-14 10:36:44 +0800

    主方法

--- src/main/java/com/example/busniess/BusniessApplication.java
+++ src/main/java/com/example/busniess/BusniessApplication.java
@@ -3,6 +3,11 @@ package com.example.busniess;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
 @MapperScan("com.example.busniess.dao")
 @SpringBootApplication
 public class BusniessApplication {
@@ -11,4 +16,7 @@ public class BusniessApplication {
 		SpringApplication.run(BusniessApplication.class, args);
 	}
 
+
 }
+
+
