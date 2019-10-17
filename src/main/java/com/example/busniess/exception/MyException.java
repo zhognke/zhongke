fa3c80071c19292eb6ae934f09aa3dbe90c427cc
fjs1987 <yangying2762@qq.com>
2019-10-17 19:05:05 +0800

    自定义错误

--- src/main/java/com/example/busniess/exception/MyException.java
+++ src/main/java/com/example/busniess/exception/MyException.java
@@ -1,12 +1,10 @@
 package com.example.busniess.exception;
 
 import com.example.busniess.resultpackage.CodeMsg;
-import com.sun.org.apache.bcel.internal.classfile.Code;
-import lombok.AllArgsConstructor;
+
 import lombok.Data;
 
 @Data
-
 public class MyException extends Exception{
 
 
