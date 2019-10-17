cfd059bf6391181c1f0d8f2c0aee2b3a0aaa8e26
fjs1987 <yangying2762@qq.com>
2019-10-17 15:09:11 +0800

    备份

--- src/main/java/com/example/busniess/entity/User.java
+++ src/main/java/com/example/busniess/entity/User.java
@@ -13,14 +13,16 @@ import java.util.Date;
 public class User implements Serializable {
     @NotNull(message = "id参数不能为空", groups = UserValidator.UpDate.class)
     private Integer id;
-    @NotNull(message = "账号不能为空", groups = {UserValidator.InSet.class})
+    @NotNull(message = "用户角色不能为空",groups =UserValidator.InSet.class)
+    private  String userRole;//用户角色
+    @NotNull(message = "账号不能为空", groups = {UserValidator.UpDate.class,UserValidator.InSet.class})
     private String userName;//用户账号
-    @NotNull(message = "密码不能为空", groups = {UserValidator.InSet.class})
-    @Length(min = 6, max = 20, message = "密码长度大于6小于20", groups = {UserValidator.InSet.class})
+    @NotNull(message = "密码不能为空", groups = {UserValidator.UpDate.class,UserValidator.InSet.class})
+    @Length(min = 6, max = 20, message = "密码长度大于6小于20", groups = {UserValidator.UpDate.class,UserValidator.InSet.class})
     private String password;//密码
-    @Pattern(regexp = "^1([38][0-9]|4[579]|5[0-3,5-9]|6[6]|7[0135678]|9[89])\\d{8}$", message = "手机号码格式错误", groups = {UserValidator.InSet.class})
+    @Pattern(regexp = "^1([38][0-9]|4[579]|5[0-3,5-9]|6[6]|7[0135678]|9[89])\\d{8}$", message = "手机号码格式错误", groups = {UserValidator.UpDate.class,UserValidator.InSet.class})
     private String phoneNumber;//手机号
-    @Email(message = "请填写正确的邮箱格式")
+    @Email(message = "请填写正确的邮箱格式",groups = {UserValidator.UpDate.class,UserValidator.InSet.class})
     private String email;//邮箱
     private String statu;//用户状态备用
     private Date lastdate;
