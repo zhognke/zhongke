4bcdd6a14a6b0e5bac4ad379311208431244bedb
fjs1987 <yangying2762@qq.com>
2019-11-15 10:52:09 +0800

    查找的方法

--- src/main/java/com/example/busniess/entity/Reject.java
+++ src/main/java/com/example/busniess/entity/Reject.java
@@ -13,6 +13,8 @@ public class Reject implements Serializable {
     private Integer id;//主id
     @NotNull(message = "关联企业信息id不能为空", groups = {UserValidator.UpDate.class,UserValidator.InSet.class})
     private Integer bId;//关联的企业认证信息id
+    @NotNull(message = "关私人信息id不能为空", groups = {UserValidator.UpDate.class,UserValidator.InSet.class})
+    private Integer PId;//关联的私人认证
     @NotBlank(message = "驳回内容不能为空", groups = {UserValidator.UpDate.class,UserValidator.InSet.class})
     private String content;//驳回的内容
     private Date rejectTime;//驳回时间
