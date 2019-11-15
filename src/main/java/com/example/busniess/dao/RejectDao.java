ca10459a9cd6933db9b9ad9aff7a6969eae10804
fjs1987 <yangying2762@qq.com>
2019-11-15 10:53:44 +0800

    查找认证

--- src/main/java/com/example/busniess/dao/RejectDao.java
+++ src/main/java/com/example/busniess/dao/RejectDao.java
@@ -1,6 +1,7 @@
 package com.example.busniess.dao;
 
 import com.example.busniess.entity.BusinessCenter;
+import com.example.busniess.entity.Person;
 import com.example.busniess.entity.Reject;
 import org.apache.ibatis.annotations.*;
 
@@ -26,6 +27,14 @@ public interface RejectDao {
     @Select("SELECT * from reject WHERE bid=#{bId}")
     public Reject selectReject(Insert bId);
 
+    /**
+     * 根据pid查询驳回理由私人验证
+     * @param bId
+     * @return
+     */
+    @Select("SELECT * from reject WHERE pid=#{pId}")
+    public Reject selectRejectpByPerson(Insert bId);
+
 
     /**
      *查询企业认证信息以及驳回原因
@@ -35,6 +44,15 @@ public interface RejectDao {
             @Result(property = "reject", column = "id", one = @One(select = "com.example.busniess.dao.RejectDao.selectReject"))
     })
     public BusinessCenter selectBussinessAndReject(@Param("userName") String userName,@Param("statue") Integer statue);
+    /**
+     *查询私人认证信息以及驳回原因
+     */
+    @Select("SELECT * from businesscenter WHERE uname=#{userName} AND statue=#{statue}")
+    @Results({
+            @Result(property = "reject", column = "id", one = @One(select = "com.example.busniess.dao.RejectDao.selectRejectpByPerson"))
+    })
+    public Person selectPersonAndReject(@Param("userName") String userName, @Param("statue") Integer statue);
+
 
 
 }
