82928546cb74e8fe08a7b6ec54d2fac7b4492e23
fjs1987 <yangying2762@qq.com>
2019-11-19 11:38:24 +0800

    企业中心

---
+++ src/main/java/com/example/busniess/entity/InformEntity.java
@@ -0,0 +1,14 @@
+package com.example.busniess.entity;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+@Data
+public class InformEntity{
+    private  String userName;//用户名
+    private String count;//内容
+    private String time;//时间
+
+
+}
