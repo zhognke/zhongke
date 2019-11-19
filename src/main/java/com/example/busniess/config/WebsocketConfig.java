9b59a68b5ed46f0cd5343d11651cc35ca0f35017
fjs1987 <yangying2762@qq.com>
2019-11-19 11:36:15 +0800

    企业中心

--- src/main/java/com/example/busniess/config/WebsocketConfig.java
+++ src/main/java/com/example/busniess/config/WebsocketConfig.java
@@ -6,6 +6,7 @@ import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @Configuration
 public class WebsocketConfig {
+    //websocket
     @Bean
     public ServerEndpointExporter serverEndpointExporter() {
         return new ServerEndpointExporter();
