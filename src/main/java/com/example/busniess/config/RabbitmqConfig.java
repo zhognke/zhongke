7565298a48154dac7a8b4f1ddcd2eb80b8919de7
fjs1987 <yangying2762@qq.com>
2019-11-19 11:34:58 +0800

    企业中心

---
+++ src/main/java/com/example/busniess/config/RabbitmqConfig.java
@@ -0,0 +1,58 @@
+package com.example.busniess.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitmqConfig {
+
+
+    @Bean
+    public MessageConverter converterMessage() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    /**
+     * 交换器
+     *
+     * @return
+     */
+    @Bean("exchange")
+    public DirectExchange creatExchange() {
+        return new DirectExchange("inform");
+    }
+
+    /**
+     * 用户队列
+     *
+     * @return
+     */
+    @Bean(name = "user")
+    public Queue creatUserQueue() {
+        return new Queue("user");
+    }
+
+    /**
+     * 管理员队列
+     *
+     * @return
+     */
+    @Bean("Administrator")
+    public Queue creatAdministratorQueue() {
+        return new Queue("Administrator");
+    }
+
+    @Bean
+    public Binding bingUser(@Qualifier("user") Queue queue, @Qualifier("exchange") DirectExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with("user");
+    }
+
+    @Bean
+    public Binding bingAdmin(@Qualifier("Administrator") Queue queue, @Qualifier("exchange") DirectExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with("admin");
+    }
+}
