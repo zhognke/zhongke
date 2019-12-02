d8543f3c73228f12ad3846c1bff4c8015f22e463
fjs1987 <yangying2762@qq.com>
2019-12-02 18:49:21 +0800

    新闻接口

--- src/main/java/com/example/busniess/entity/NewsInformation.java
+++ src/main/java/com/example/busniess/entity/NewsInformation.java
@@ -12,6 +12,7 @@ import java.util.Date;
 @Data
 
 public class NewsInformation implements Serializable {
+
     @NotNull(message = "id参数不能为空", groups = UserValidator.UpDate.class)
     private int id; //新闻资讯id号
     @NotBlank(message = "用户名不能为空", groups = UserValidator.UpDate.class)
@@ -23,8 +24,7 @@ public class NewsInformation implements Serializable {
     private  String resource;//来源
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private  Date publishDate;//发布时间
-
-//    @NotBlank(message = "封面地址不能为空", groups = {UserValidator.UpDate.class, UserValidator.InSet.class})
+//  @NotBlank(message = "封面地址不能为空", groups = {UserValidator.UpDate.class, UserValidator.InSet.class})
     private String cover;//封面地址
     @NotBlank(message = "新闻详情不能为空", groups = {UserValidator.UpDate.class, UserValidator.InSet.class})
     private String detail;//新闻详情
